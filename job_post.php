6ba86733b1b0e17c8e08a22d309ada5b0ddf3b46
littlefrank <ochinfrank@gmail.com>
2016-10-22 21:25:12 +0100

    jobpost

---
+++ job_post.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<?php require 'static/_head.inc.php';?>
+<body class="home">
+
+    <!-- Wrapper -->
+    <div id="wrapper" class="wide">   
+
+        <?php require 'static/_header.inc.php'; ?>
+
+<style>
+#copyright{
+    bottom: 0px;
+    position: absolute;
+    margin: 0 auto;
+    width: 100%
+ }
+
+ </style>
+
+        <div class="page-content">
+			<!-- Section -->
+			<section>
+
+				<div class="container">
+					<div class="row">
+						<div class="col col_12_of_12">
+
+							<div class="job_post_form">
+								<form class="" method="POST" action="">
+									<h2>Post Job</h2>
+									<div class="form-group">
+										<div class="p">
+										<label>Job Discription:</label>
+										<input class="" value="" name="" type="text" placeholder=""></input>
+										</div>
+									</div>
+									<div class="form-db-group">
+										<div class="p">
+										<label>Choose an Industry:</label>
+										<br />
+										<input class="" value="" name="" type="text" placeholder=""></input>
+										</div>
+										<div class="p">
+										<label>Choose a Category:</label>
+										<br />
+										<input class="" value="" name="" type="text" placeholder=""></input>
+										</div>
+									</div>
+									<div class="form-group">
+										<label>When would you like your service delivered?</label>
+										<input class="" value="" name="" type="text" placeholder=""></input>
+									</div>
+									<div class="form-group">
+										<label>What is your budget for this service? (Optional) </label>
+										<input class="" value="" name="" type="text" placeholder=""></input>
+									</div>
+									<input class="" value="Post" type="submit"></input>
+								</form>
+
+							</div>				
+							
+						</div>					
+							
+						</div>
+
+					</div>
+				</div>
+			</section><!-- End Section -->
+		</div>
+
+
+		<?php require 'static/_copyright.inc.php'; ?>
+
+    </div><!-- End Wrapper -->
+    
+    <?php require 'static/_scripts.inc.php'; ?>   
+
+</body>
+</html>
